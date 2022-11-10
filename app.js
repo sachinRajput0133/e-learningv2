bbebfc80fc2a6dbaaea0359c7dbf4b0f45c6cbc1
sachin <sachinrajput0133@gmail.com>
2022-11-10 13:00:46 +0530

    port

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
 import express from "express";
 import ErrorMiddleware from "./middlewares/Error.js";
-import courseRouter from "./routes/CouseRoutes.js"
-import userRouter from "./routes/UserRoutes.js"
-import paymentRoute from "./routes/PaymentRoutes.js"
-import otherRoute from "./routes/OtherRoutes.js"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-dotenv.config({path:"./config/config.env"})
+import courseRouter from "./routes/CouseRoutes.js";
+import userRouter from "./routes/UserRoutes.js";
+import paymentRoute from "./routes/PaymentRoutes.js";
+import otherRoute from "./routes/OtherRoutes.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config({ path: "./config/config.env" });
 
-const app=express();
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
-app.use(cors({
-origin:process.env.FRONTEND_URL,
-credentials:true,               //OTHERWISE WON'T BE ABLE TO USE COOKIE
-methods:["GET","POST","PUT","DELETE"]
-}))
-app.use("/api/v1",courseRouter)
-app.use("/api/v1",userRouter)
-app.use("/api/v1",paymentRoute)
-app.use("/api/v1",otherRoute)
-
-
-
-
-
-
-
-
-
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true, //OTHERWISE WON'T BE ABLE TO USE COOKIE
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use("/api/v1", courseRouter);
+app.use("/api/v1", userRouter);
+app.use("/api/v1", paymentRoute);
+app.use("/api/v1", otherRoute);
 
 export default app;
-app.get("/",(req,res)=>res.send(`<h1>site is working. click <a href=${process.env.FRONTEND_URL}> here </a> to visit frontend</h1>`))
+app.get("/", (req, res) =>
+  res.send(
+    `<h1>site is working. click <a href=${process.env.FRONTEND_URL}> here </a> to visit frontend</h1>`
+  )
+);
 
-// app.get("/",(req,res)=>res.send("<h1>site is working. click <a href=${process.env.frontendurl} to visit frontend</h1>))
-
-app.use(ErrorMiddleware)
+app.use(ErrorMiddleware);
