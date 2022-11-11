1c45b58e191046a6d65833330807980e09ac76c0
sachin <sachinrajput0133@gmail.com>
2022-11-12 00:07:46 +0530

    cors

--- app.js
+++ app.js
@@ -13,21 +13,29 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    // origin: [process.env.FRONTEND_URL,'https://api.razorpay.com'],
-    origin:[process.env.FRONTEND_URL,'https://api.razorpay.com','https://checkout-static.razorpay.com/build/c15c2810f99ef3fd51cbbe1a20598e286eff50c1/checkout-frame.js'],
-    // AccessControlAllowOrigin:"*",
-    credentials: true, //OTHERWISE WON'T BE ABLE TO USE COOKIE
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-// app.use((req, res, next) => {
-//    res.header("Access-Control-Allow-Origin", 'https://api.razorpay.com');
-//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//    next();
-// }) 
+// app.use(
+//   cors({
+  
+//     origin:[process.env.FRONTEND_URL],
+   
+//     credentials: true, //OTHERWISE WON'T BE ABLE TO USE COOKIE
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+const whitelist = [process.env.FRONTEND_URL,'https://api.razorpay.com','https://checkout-static.razorpay.com/build/c15c2810f99ef3fd51cbbe1a20598e286eff50c1/checkout-frame.js']
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true , credentials: true, methods: ["GET", "POST", "PUT", "DELETE"],} // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+ 
+app.get('/products/:id', cors(corsOptionsDelegate), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
+})
 app.use("/api/v1", courseRouter);
 app.use("/api/v1", userRouter);
 app.use("/api/v1", paymentRoute);
