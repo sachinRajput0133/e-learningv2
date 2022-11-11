3582526b594b6849409ce824965d21e214fcdcda
sachin <sachinrajput0133@gmail.com>
2022-11-12 00:17:08 +0530

    cors

--- app.js
+++ app.js
@@ -13,25 +13,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(
-//   cors({
+app.use(
+  cors({
   
-//     origin:[process.env.FRONTEND_URL],
+    origin:[process.env.FRONTEND_URL],
    
-//     credentials: true, //OTHERWISE WON'T BE ABLE TO USE COOKIE
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
-const whitelist = [process.env.FRONTEND_URL,'https://api.razorpay.com','https://checkout-static.razorpay.com/build/c15c2810f99ef3fd51cbbe1a20598e286eff50c1/checkout-frame.js']
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true , credentials: true, methods: ["GET", "POST", "PUT", "DELETE"],} // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+    credentials: true, //OTHERWISE WON'T BE ABLE TO USE COOKIE
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
  
 app.get('/products/:id', cors(corsOptionsDelegate), function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
