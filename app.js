9f43c6d183dc27a06b2b3fdcb084372687bda5e5
sachin <sachinrajput0133@gmail.com>
2022-11-14 00:09:43 +0530

    cokierem

--- app.js
+++ app.js
@@ -13,15 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-   
-    origin:[process.env.FRONTEND_URL],
-   
-    credentials: true, //OTHERWISE WON'T BE ABLE TO USE COOKIE
-    methods: ["GET", "POST", "PUT", "DELETE","HEAD"],
-  })
-);
 // app.use(
 //   cors({
    
@@ -31,6 +22,27 @@ app.use(
 //     methods: ["GET", "POST", "PUT", "DELETE","HEAD"],
 //   })
 // );
+var allowedOrigins = [process.env.FRONTEND_URL,
+                      'https://api.razorpay.com'];
+app.use(
+  cors({
+   
+    origin:function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+   
+    credentials: true, //OTHERWISE WON'T BE ABLE TO USE COOKIE
+    methods: ["GET", "POST", "PUT", "DELETE","HEAD"],
+  })
+);
 
  
 
